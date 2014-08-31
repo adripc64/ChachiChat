00c0ceea4efa76bd6d398a07bfd38d9bfbfbeeaa
adripc64 <adripc64@gmail.com>
2014-08-31 17:26:58 +0200

    Constructuro del servidor

--- src/chachichat/server/ChatServer.java
+++ src/chachichat/server/ChatServer.java
@@ -1,5 +1,23 @@
 package chachichat.server;
 
+import java.io.IOException;
+
+import com.esotericsoftware.kryonet.Server;
+
 public class ChatServer {
+	
+	private int port;
+	private Server server;
+	
+	public ChatServer(int port) throws IOException {
+		
+		this.port = port;
+		
+		server = new Server();
+		server.start();
+		server.bind(port);
+
+		
+	}
 		
 }
