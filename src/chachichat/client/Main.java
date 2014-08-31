4c0ce418fe6b2ec507fd944938decabb561960cd
Elite Legend <Elite Legend@192.168.1.35>
2014-08-31 18:03:58 +0200

    Connect Main

--- src/chachichat/client/Main.java
+++ src/chachichat/client/Main.java
@@ -1,14 +1,24 @@
 package chachichat.client;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("Introduzca la ip a la que desea conectarse: ");
 		String ip = sc.next();
-		System.out.println("Conectando a la ip " + ip);
+		int puerto = 7777;
+		
+		try {
+			ChatClient cliente = new ChatClient(ip, puerto);
+			System.out.println("Conectando a la ip " + ip);
+		} catch (IOException e) {
+			System.out.println("Unable to connect to "+ip);
+		}
+
 		sc.close();
 	}
 
